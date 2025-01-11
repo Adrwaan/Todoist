c80ad96ff39eeabf9b41de83b629d799c8a5c6ff
Adrwaan <adrean.danilo113@gmail.com>
2025-01-11 15:58:20 +0000

    chore: tailwindCSS setup / comments on vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,13 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
+  // Assign .env file a process.env object
   process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ""));
 
   return {
     plugins: [react()],
     server: {
-      host: process.env.HOST,
+      host: process.env.HOST, // Only in development environment.
     },
   };
 });
